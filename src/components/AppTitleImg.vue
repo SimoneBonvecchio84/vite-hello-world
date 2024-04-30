6d7ef1191abb021cb35e23155ed3057601b13739
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-30 20:20:01 +0200

    Feat: add img

--- src/components/AppTitleImg.vue
+++ src/components/AppTitleImg.vue
@@ -10,8 +10,14 @@ export default {
 
 <template>
     <h2>{{ title }}</h2>
+    <div class="conatiner">
+        <img src="../assets/img/simo.jpg" alt="">
+    </div>
+
 </template>
 
 <style>
-
+    .container img {
+        width: 100%;
+    }
 </style>
